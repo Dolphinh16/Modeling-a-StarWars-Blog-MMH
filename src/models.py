cd8ffcd92dbcf29a974f546dcff289291d93189a
Dolphinh16 <miguelhmercedes@hotmail.com>
2025-05-20 16:29:28 +0000

    ModelingStarWarsMMH

--- src/models.py
+++ src/models.py
@@ -1,14 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(20), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(20), nullable=False)
+    username: Mapped[str] = mapped_column(String(30), nullable=False)
 
 
     def serialize(self):
@@ -17,3 +18,24 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Personaje (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    descripcion: Mapped[str] = mapped_column(String(200), nullable=False)
+
+class Planeta (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    descripcion: Mapped[str] = mapped_column(String(200), nullable=False)
+
+class Favorito (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int]= mapped_column(ForeignKey("user.id"))
+    user=relationship("User" , backref="favoritos")
+    personaje_id: Mapped[int]= mapped_column(ForeignKey("personaje.id"))
+    personaje= relationship("Personaje" , backref="favoritos")
+    planeta_id: Mapped[int]= mapped_column(ForeignKey("planeta.id"))
+    planeta = relationship("Planeta" , backref="favoritos")
+
+
